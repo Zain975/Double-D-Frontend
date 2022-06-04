d9b1d5e5fa03d9243830e19b1f6a09ae3e8ceeb4
Zain975 <77121307+Zain975@users.noreply.github.com>
2022-06-04 19:30:40 +0500

    Update MediaPage.js

--- src/pages/media/MediaPage.js
+++ src/pages/media/MediaPage.js
@@ -28,7 +28,9 @@ function MediaPage() {
   return (
     <div
       className='media'
-      style={{ backgroundImage: `url(${background.imgUrl})` }}
+      style={{ backgroundImage: `url(${background.imgUrl})`,
+        height: "1080px",
+        width: "5760px", }}
     >
       <Link to='/menu'>
         <div className='back'>
